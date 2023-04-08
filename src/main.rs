d483ca5792a8e14f3a977b62ba3bd5626bd0f3c2
Jacob Skoog <jacob.skoog@pm.me>
2023-04-08 12:50:31 +0200

    removing debug

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use std::collections::HashSet;
 use oxibloom::{bloom, os_random};
 
 fn main() {
     let mut og_values: Vec<u128> = Vec::with_capacity(1_000_000);
     let mut bloom_filter = bloom::BloomFilter::new(1_000_000, 0.01);
-    println!("bloom debug: {bloom_filter:?}");
     for _ in 0..1_000_000 {
         let r = os_random::get_random_u128();
         og_values.push(r.clone());
@@ -15,5 +13,4 @@ fn main() {
             panic!("it should be contained!")
         }
     }
-    println!("bloom debug #2: {bloom_filter:?}");
 }
